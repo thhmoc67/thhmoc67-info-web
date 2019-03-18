3946910143073507675a907c9116ec5722c02000
Dayal <dayal.sharan@nestaway.com>
2019-03-18 19:13:41 +0530

    webpack setting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    console.log('yeah')
     return (
       <div className="App">
         <h1> Hello, World! </h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,93 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require("path"),
+  webpack = require("webpack"),
+  AssetsPlugin = require("assets-webpack-plugin"),
+  BrotliPlugin = require("brotli-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const isProd = process.env.NODE_ENV == "production";
+
+/**
+ * Plugins for dev environment
+ */
+const devPlugins = [
+  new HtmlWebpackPlugin({
+    template: "./public/index.html",
+    title: "thhmoc67-info-web",
+  }),
+  new AssetsPlugin({
+    prettyPrint: true,
+    filename: "assets.json",
+    path: path.resolve(__dirname, "build"),
+  }),
+  new webpack.DefinePlugin({
+    __ENV__: JSON.stringify(process.env.NODE_ENV || "development"),
+  }),
+];
+/**
+ * Plugins for production environment
+ */
+const prodPlugins = [
+  new BrotliPlugin({
+    asset: "[path].br[query]",
+    test: /\.(js|css|html|svg)$/,
+    threshold: 10240,
+    minRatio: 0.8,
+  }),
+  new UglifyJsPlugin({
+    cache: true,
+    parallel: true,
+    sourceMap: true,
+  }),
+];
+/**
+ * Merging plugins on the basis of env
+ */
+const pluginList = isProd ? [...devPlugins, ...prodPlugins] : devPlugins;
 
 module.exports = {
+  // May add cheap-module-source-map to devtool to generate source maps to prod builds
+  devtool: isProd ? "" : "inline-source-map",
   entry: "./src/index.js",
-  mode: "development",
+  output: {
+    filename: isProd ? "[name].[chunkhash].js" : "[name].bundle.js",
+    path: path.resolve(__dirname, "build/client"),
+    publicPath: "build/client/",
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.js$/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        exclude: /node_modules/,
+        options: { presets: ["@babel/env"] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: pluginList,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+    runtimeChunk: {
+      name: "manifest",
+    },
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    publicPath: "http://localhost:3000/build/",
+    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
